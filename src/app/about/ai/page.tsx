e320c37d537e0435d82d4b767e47505db73e9c24
“Jaotar” <Pornpailinfanta@gmail.com>
2024-07-19 14:43:24 +0700

    first commit

---
+++ src/app/about/ai/page.tsx
@@ -0,0 +1,11 @@
+import AI from "@/components/AI";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      page
+      <AI />
+    </div>
+  );
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import About from "@/components/About";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      page
+      <About />
+    </div>
+  );
+}
